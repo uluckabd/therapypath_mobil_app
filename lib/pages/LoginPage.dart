03fe780b3b23fbf2183e949a1897076d989dfdb0
Abdullah ULUCAK <ulucakabdullahulucak@gmail.com>
2025-09-11 14:35:05 +0300

    üye ol sayfası tasarlandı ve sayfalar birbirine göre konumlandırıldı

--- lib/pages/LoginPage.dart
+++ lib/pages/LoginPage.dart
@@ -31,143 +31,153 @@ class _LoginState extends State<Login> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(60, 65, 242, 37),
-      body: Center(
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
+      body: Stack(
+        children: [
+          // Logo ve başlığı sabit bir konuma yerleştirir
+          Positioned(
+            top: 100, // Ekranın üstünden 100 piksel aşağıda
+            left: 0,
+            right: 0,
+            child: Column(
+              children: [
+                const Text(
                   "THERAPYPATH",
                   style: TextStyle(
                     fontSize: 40,
                     fontWeight: FontWeight.bold,
                     color: Color(0xFFF7F3E9),
                   ),
+                  textAlign: TextAlign.center,
                 ),
-              ),
+                const SizedBox(height: 20),
+                Image.asset("Images/Loginimage.png", width: 180, height: 180),
+              ],
+            ),
+          ),
 
-              // ----------------- Yuvarlak Logo -----------------
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Image.asset("Images/Loginimage.png", width: 180),
-              ),
-
-              const SizedBox(height: 90),
-
-              // ----------------- TextField'lar -----------------
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 40.0),
-                child: TextField(
-                  controller: _usernameController,
-                  style: const TextStyle(
-                    fontSize: 15,
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold,
-                  ),
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    filled: true,
-                    fillColor: Color(0xFFF7F3E9),
-                    hintText: "Adınızı giriniz",
-                    hintStyle: const TextStyle(color: Colors.grey),
-                    contentPadding: const EdgeInsets.symmetric(
-                      vertical: 18,
-                      horizontal: 20,
-                    ),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 16),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 40.0),
-                child: TextField(
-                  controller: _passwordController,
-                  style: const TextStyle(
-                    fontSize: 15,
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold,
-                  ),
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    filled: true,
-                    fillColor: Color(0xFFF7F3E9),
-                    hintText: "Şifrenizi giriniz",
-                    hintStyle: const TextStyle(color: Colors.grey),
-                    contentPadding: const EdgeInsets.symmetric(
-                      vertical: 18,
-                      horizontal: 20,
-                    ),
-                  ),
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
+          // Geri kalan içeriği aşağıya kaydırır ve SingleChildScrollView ile sarar
+          Padding(
+            padding: const EdgeInsets.only(
+              top: 350.0,
+            ), // Logo ve başlığın yüksekliğine göre bir boşluk bırak
+            child: SingleChildScrollView(
+              child: Column(
                 children: [
-                  const SizedBox(width: 40),
-                  Checkbox(
-                    value: _rememberMe,
-                    onChanged: (newvalue) {
-                      setState(() {
-                        _rememberMe = newvalue ?? false;
-                      });
-                    },
-                    // Checkbox'ın rengini belirleyen özellik
-                    side: const BorderSide(
-                      color: Color(0xFFF7F3E9), // Çerçeve rengini ayarlar
-                      width: 2.0, // Çerçeve kalınlığını ayarlar
+                  const SizedBox(height: 90),
+                  // ----------------- TextField'lar -----------------
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 40.0),
+                    child: TextField(
+                      controller: _usernameController,
+                      style: const TextStyle(
+                        fontSize: 15,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        filled: true,
+                        fillColor: const Color(0xFFF7F3E9),
+                        hintText: "Adınızı giriniz",
+                        hintStyle: const TextStyle(color: Colors.grey),
+                        contentPadding: const EdgeInsets.symmetric(
+                          vertical: 18,
+                          horizontal: 20,
+                        ),
+                      ),
                     ),
-                    activeColor: Color(
-                      0xFFF7F3E9,
-                    ), // Seçili olduğunda içini dolduracak tik rengi
-                    checkColor: Color(0xFF295C7C), // Tik işaretinin rengi
                   ),
-                  const Text(
-                    "Beni hatırla",
-                    style: TextStyle(color: Color(0xFFF7F3E9)),
+                  const SizedBox(height: 16),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 40.0),
+                    child: TextField(
+                      controller: _passwordController,
+                      style: const TextStyle(
+                        fontSize: 15,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        filled: true,
+                        fillColor: const Color(0xFFF7F3E9),
+                        hintText: "Şifrenizi giriniz",
+                        hintStyle: const TextStyle(color: Colors.grey),
+                        contentPadding: const EdgeInsets.symmetric(
+                          vertical: 18,
+                          horizontal: 20,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      const SizedBox(width: 40),
+                      Checkbox(
+                        value: _rememberMe,
+                        onChanged: (newvalue) {
+                          setState(() {
+                            _rememberMe = newvalue ?? false;
+                          });
+                        },
+                        // Checkbox'ın rengini belirleyen özellik
+                        side: const BorderSide(
+                          color: Color(0xFFF7F3E9), // Çerçeve rengini ayarlar
+                          width: 2.0, // Çerçeve kalınlığını ayarlar
+                        ),
+                        activeColor: const Color(
+                          0xFFF7F3E9,
+                        ), // Seçili olduğunda içini dolduracak tik rengi
+                        checkColor: const Color(
+                          0xFF295C7C,
+                        ), // Tik işaretinin rengi
+                      ),
+                      const Text(
+                        "Beni hatırla",
+                        style: TextStyle(color: Color(0xFFF7F3E9)),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 40),
+                  ElevatedButton(
+                    onPressed: _login,
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFFF7F3E9),
+                    ),
+                    child: const Text(
+                      "Giriş Yap",
+                      style: TextStyle(color: Color(0xFF295C7C)),
+                    ),
+                  ),
+                  const SizedBox(height: 15),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFFF7F3E9),
+                    ),
+                    child: const Text(
+                      "Üye Ol",
+                      style: TextStyle(color: Color(0xFF295C7C)),
+                    ),
+                  ),
+                  const SizedBox(height: 25),
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      "Yardım",
+                      style: TextStyle(color: Color(0xFFF7F3E9)),
+                    ),
                   ),
                 ],
               ),
-
-              const SizedBox(height: 40),
-
-              ElevatedButton(
-                onPressed: _login,
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Color(0xFFF7F3E9),
-                ),
-                child: const Text(
-                  "Giriş Yap",
-                  style: TextStyle(color: Color(0xFF295C7C)),
-                ),
-              ),
-              const SizedBox(height: 15),
-              ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Color(0xFFF7F3E9),
-                ),
-                child: const Text(
-                  "Üye Ol",
-                  style: TextStyle(color: Color(0xFF295C7C)),
-                ),
-              ),
-              const SizedBox(height: 25),
-              TextButton(
-                onPressed: () {},
-                child: const Text(
-                  "Yardım",
-                  style: TextStyle(color: Color(0xFFF7F3E9)),
-                ),
-              ),
-            ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
--- lib/pages/SignupPage.dart
+++ lib/pages/SignupPage.dart
@@ -12,136 +12,144 @@ class _SignuppageState extends State<Signuppage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(60, 65, 242, 37),
-      body: Center(
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(4.0),
-                child: Text(
+      body: Stack(
+        children: [
+          // Logo ve başlığı sabit bir konuma yerleştirir
+          Positioned(
+            top: 100, // Ekranın üstünden 100 piksel aşağıda
+            left: 0,
+            right: 0,
+            child: Column(
+              children: [
+                const Text(
                   "THERAPYPATH",
                   style: TextStyle(
                     fontSize: 40,
                     fontWeight: FontWeight.bold,
                     color: Color(0xFFF7F3E9),
                   ),
+                  textAlign: TextAlign.center,
                 ),
-              ),
-              SizedBox(height: 40),
+                const SizedBox(height: 20),
+                Image.asset("Images/Signupimage.png", width: 180, height: 180),
+              ],
+            ),
+          ),
 
-              // ----------------- Yuvarlak Logo -----------------
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Image.asset("Images/Signupimage.png", width: 180),
-              ),
-
-              const SizedBox(height: 90),
-
-              // ----------------- TextField'lar -----------------
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 40.0),
-                child: TextField(
-                  style: const TextStyle(
-                    fontSize: 15,
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold,
-                  ),
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    filled: true,
-                    fillColor: Color(0xFFF7F3E9),
-                    hintText: "kullanıcı adını giriniz",
-                    hintStyle: const TextStyle(color: Colors.grey),
-                    contentPadding: const EdgeInsets.symmetric(
-                      vertical: 18,
-                      horizontal: 20,
-                    ),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 16),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 40.0),
-                child: TextField(
-                  style: const TextStyle(
-                    fontSize: 15,
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold,
-                  ),
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    filled: true,
-                    fillColor: Color(0xFFF7F3E9),
-                    hintText: "Şifrenizi giriniz",
-                    hintStyle: const TextStyle(color: Colors.grey),
-                    contentPadding: const EdgeInsets.symmetric(
-                      vertical: 18,
-                      horizontal: 20,
-                    ),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 16),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 40.0),
-                child: TextField(
-                  style: const TextStyle(
-                    fontSize: 15,
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold,
-                  ),
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    filled: true,
-                    fillColor: Color(0xFFF7F3E9),
-                    hintText: "Şifrenizi tekrar giriniz",
-                    hintStyle: const TextStyle(color: Colors.grey),
-                    contentPadding: const EdgeInsets.symmetric(
-                      vertical: 18,
-                      horizontal: 20,
-                    ),
-                  ),
-                ),
-              ),
-
-              const SizedBox(height: 40),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
+          // Geri kalan içeriği aşağıya kaydırır ve SingleChildScrollView ile sarar
+          Padding(
+            padding: const EdgeInsets.only(
+              top: 350.0,
+            ), // Logo ve başlığın yüksekliğine göre bir boşluk bırak
+            child: SingleChildScrollView(
+              child: Column(
                 children: [
-                  ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Color(0xFFF7F3E9),
-                    ),
-                    child: const Text(
-                      "iptal",
-                      style: TextStyle(color: Color(0xFF295C7C)),
+                  const SizedBox(height: 90),
+                  // ----------------- TextField'lar -----------------
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 40.0),
+                    child: TextField(
+                      style: const TextStyle(
+                        fontSize: 15,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        filled: true,
+                        fillColor: const Color(0xFFF7F3E9),
+                        hintText: "kullanıcı adını giriniz",
+                        hintStyle: const TextStyle(color: Colors.grey),
+                        contentPadding: const EdgeInsets.symmetric(
+                          vertical: 18,
+                          horizontal: 20,
+                        ),
+                      ),
                     ),
                   ),
-                  const SizedBox(width: 30),
-                  ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Color(0xFFF7F3E9),
+                  const SizedBox(height: 16),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 40.0),
+                    child: TextField(
+                      style: const TextStyle(
+                        fontSize: 15,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        filled: true,
+                        fillColor: const Color(0xFFF7F3E9),
+                        hintText: "Şifrenizi giriniz",
+                        hintStyle: const TextStyle(color: Colors.grey),
+                        contentPadding: const EdgeInsets.symmetric(
+                          vertical: 18,
+                          horizontal: 20,
+                        ),
+                      ),
                     ),
-                    child: const Text(
-                      "kayıt  Ol",
-                      style: TextStyle(color: Color(0xFF295C7C)),
+                  ),
+                  const SizedBox(height: 16),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 40.0),
+                    child: TextField(
+                      style: const TextStyle(
+                        fontSize: 15,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        filled: true,
+                        fillColor: const Color(0xFFF7F3E9),
+                        hintText: "Şifrenizi tekrar giriniz",
+                        hintStyle: const TextStyle(color: Colors.grey),
+                        contentPadding: const EdgeInsets.symmetric(
+                          vertical: 18,
+                          horizontal: 20,
+                        ),
+                      ),
                     ),
+                  ),
+                  const SizedBox(height: 40),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color(0xFFF7F3E9),
+                        ),
+                        child: const Text(
+                          "iptal",
+                          style: TextStyle(color: Color(0xFF295C7C)),
+                        ),
+                      ),
+                      const SizedBox(width: 30),
+                      ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color(0xFFF7F3E9),
+                        ),
+                        child: const Text(
+                          "kayıt Ol",
+                          style: TextStyle(color: Color(0xFF295C7C)),
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
-            ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
